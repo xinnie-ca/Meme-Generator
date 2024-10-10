95c7d087e1882e3a24fdd9d2c66a490c50d0d4ab
XinNie <umnie2@myumanitoba.ca>
2024-10-10 15:12:53 -0500

    add input label, associate the label with box by htmlfor

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,8 +4,24 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <div>
+          <label htmlFor="top--text">Top Text</label>
+          <input
+            id="top--text"
+            type="text"
+            placeholder="Shut Up"
+            className="form--input"
+          />
+        </div>
+        <div>
+          <label htmlFor="bottom--text">Bottom Text</label>
+          <input
+            id="bottom--text"
+            type="text"
+            placeholder="and take my money"
+            className="form--input"
+          />
+        </div>
         <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
