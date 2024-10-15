df5b121883279fe7c2483c8c6400375214ebc4f4
XinNie <umnie2@myumanitoba.ca>
2024-10-15 14:43:49 -0500

    change the display of main to flex, able to display meme by randomly generate the id

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,38 +1,45 @@
 import React from "react";
 import myMeme from "../memesData";
+
 export default function Meme() {
-  function handleClick() {
+  const [image, setImage] = React.useState("");
+
+  function getMemeImage() {
     const id = Math.floor(Math.random() * (myMeme.data.memes.length - 1)) + 1;
     const meme = myMeme.data.memes[id];
     const url = meme.url;
-    return url;
+    setImage(url);
   }
 
   return (
     <main>
       <div className="form">
-        <div>
-          <label htmlFor="top--text">Top Text</label>
-          <input
-            id="top--text"
-            type="text"
-            placeholder="Shut Up"
-            className="form--input"
-          />
+        <div className="form--box">
+          <div>
+            <label htmlFor="top--text">Top Text</label>
+            <input
+              id="top--text"
+              type="text"
+              placeholder="Shut Up"
+              className="form--input"
+            />
+          </div>
+          <div>
+            <label htmlFor="bottom--text">Bottom Text</label>
+            <input
+              id="bottom--text"
+              type="text"
+              placeholder="and take my money"
+              className="form--input"
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="bottom--text">Bottom Text</label>
-          <input
-            id="bottom--text"
-            type="text"
-            placeholder="and take my money"
-            className="form--input"
-          />
-        </div>
-        <button className="form--button" onClick={handleClick}>
+        <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
+
+      <img className="meme--image" src={image} alt="Meme"></img>
     </main>
   );
 }
